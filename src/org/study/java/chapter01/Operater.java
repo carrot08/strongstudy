674cf34db156a24d5196491d81ce19439f8a488f
이승민 <waitmind08@nhnent.com>
2015-10-12 01:37:40 +0900

    operater

--- src/org/study/java/chapter01/Operater.java
+++ src/org/study/java/chapter01/Operater.java
@@ -20,7 +20,34 @@ public class Operater {
 		
 		
 		int b = 3;
+		System.out.println("=====" +b+"====");
+		System.out.println(0%b);
+		System.out.println(1%b);
+		System.out.println(2%b);
+		System.out.println(3%b);
+		System.out.println(4%b);
+		System.out.println(5%b);
+
+		int c = 6;
+		System.out.println("====="+c+"====");
+		System.out.println(0%c);
+		System.out.println(1%c);
+		System.out.println(2%c);
+		System.out.println(3%c);
+		System.out.println(4%c);
+		System.out.println(5%c);
 		
-		System.out.println(0%3);
+		int d = 1;
+		int e = 2;
+		System.out.println(d+e);
+		System.out.println(d/e);
+		
+		int f = 0x7fFFFFFF;
+		System.out.println(f);
+		System.out.println(f * f);
+		
+		float g= 0x7fFFFFFF;
+		System.out.println(f * g);
+		System.out.println((double)f * g);
 	}
 }
