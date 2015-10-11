47d67a21285651b143ac610353049df3f418a365
이승민 <waitmind08@nhnent.com>
2015-10-12 02:02:06 +0900

    public class LogicalOperation

---
+++ src/org/study/java/chapter01/LogicalOperation.java
@@ -0,0 +1,44 @@
+package org.study.java.chapter01;
+
+public class LogicalOperation {
+	public static void main(String[] args) {
+		
+		System.out.println(" == LogicalOperation == ");
+ 
+		int a = 200;
+		
+		if(true && true) {
+			System.out.println(true);
+		}
+		
+		if(true && false) {
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+
+		if(true || false) {
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+
+		if(!true) {
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+
+		if(!true && true) {
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+		
+		if((!true || true) && true) {
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+	}
+}
