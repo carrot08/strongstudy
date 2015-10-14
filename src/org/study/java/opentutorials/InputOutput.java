011b1f5de4bd82caee6fa975046a9fd7202c81a5
이승민 <waitmind08@nhnent.com>
2015-10-14 18:42:46 +0900

    함수나누기

--- src/org/study/java/opentutorials/InputOutput.java
+++ src/org/study/java/opentutorials/InputOutput.java
@@ -6,24 +6,30 @@ import java.util.Scanner;
 
 public class InputOutput {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		 System.out.println(args.length);
 		 
-		 /*
-		 Scanner sc = new Scanner(System.in);
+		 //scanner_1();
+		 scanner_2();
+		 //file_1();
+		 
+	}
+	
+	public static void scanner_1() {
+		Scanner sc = new Scanner(System.in);
 		 int i = sc.nextInt();
 		 System.out.println(i * 2);
 		 sc.close();
-		 */
-
-		 /*
-		 Scanner sc = new Scanner(System.in);
+	}
+	public static void scanner_2() {
+		Scanner sc = new Scanner(System.in);
 		 while(sc.hasNextInt()) {
 	            System.out.println(sc.nextInt()* 2); 
 	        }
 		 sc.close();
-		 */
-		 try {
+	}
+	public static void file_1() throws InterruptedException {
+		try {
             File file = new File("out.txt");
             Scanner sc = new Scanner(file);
             while(sc.hasNextInt()) {
@@ -33,8 +39,6 @@ public class InputOutput {
         } catch(FileNotFoundException e){
             e.printStackTrace();
         }
-
-	
 	}
 
 }
